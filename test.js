43e6c64ce7d8dae6a53204239b0d9d9b1b8a91aa
Stéphan Kochen <stephan@angrybytes.com>
2012-02-01 09:54:51 +0100

    Use sync calls where we can in tests.

--- test.js
+++ test.js
@@ -8,40 +8,35 @@ var yamlverse = require('./');
 var testDir = '/tmp/yamlverse-test';
 
 test('basic config reading', function(t) {
-    fs.mkdir(testDir, 0700, function(err) {
-        if (err && err.code !== 'EEXIST')
-            t.error(err, 'create test suite working directory');
-        universe.root = testDir;
+    fs.mkdirSync(testDir, 0700);
+    universe.root = testDir;
 
-        var cfgPath = path.resolve(universe.config, 'foo.yml');
-        fs.writeFile(cfgPath, 'dev: { three: 3 }', function(err) {
-            t.error(err, 'create test config file');
+    var cfgPath = path.resolve(universe.config, 'foo.yml');
+    fs.writeFileSync(cfgPath, 'dev: { three: 3 }');
 
-            yamlverse.tags = "dev";
-            t.equal(yamlverse('foo').three, 3,
-                'config data should match what was hand-written');
-            t.equal(yamlverse('foo', { five: 5 }).five, 5,
-                'defaults should apply for unspecified options');
+    yamlverse.tags = "dev";
+    t.equal(yamlverse('foo').three, 3,
+        'config data should match what was hand-written');
+    t.equal(yamlverse('foo', { five: 5 }).five, 5,
+        'defaults should apply for unspecified options');
 
-            fs.writeFile(cfgPath, 'dev: { five: 5 }', function(err) {
-                t.error(err, 'rewrite test config file');
-                t.equal(yamlverse('foo').five, undefined,
-                    'processed config should be cached');
+    fs.writeFileSync(cfgPath, 'dev: { five: 5 }');
+    t.equal(yamlverse('foo').five, undefined,
+        'processed config should be cached');
 
-                yamlverse.clearCache();
-                t.equal(yamlverse('foo').five, 5,
-                    'config should be reread after clearing cache');
+    yamlverse.clearCache();
+    t.equal(yamlverse('foo').five, 5,
+        'config should be reread after clearing cache');
 
-                yamlverse.tags = '';
-                yamlverse.clearCache();
-                t.equal(yamlverse('foo').five, undefined,
-                    'tags can be altered on the fly with a cache clear');
+    yamlverse.tags = '';
+    yamlverse.clearCache();
+    t.equal(yamlverse('foo').five, undefined,
+        'tags can be altered on the fly with a cache clear');
 
-                rimraf(testDir, function(err) {
-                    t.error(err, 'cleanup of test suite working directory');
-                    t.end();
-                });
-            });
-        });
+    rimraf(testDir, function(err) {
+        if (err)
+            throw err;
+        else
+            t.end();
     });
 });
